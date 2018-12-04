0bba47422bdbdc4489ff9f1ded3cf922c8bcf1c3
acid7820@gmail.com <acid7820@gmail.com>
2018-12-04 21:44:40 +0800

    刪除最初做showList方法的想法順序註解

--- JS/toDoList.js
+++ JS/toDoList.js
@@ -4,7 +4,6 @@ function showList(){
   //建立變數存新增代辦事項 input 的文字
   let addNewListText = document.querySelector('.task').value;
 
-
   if(addNewListText === ""){
     window.alert('请输入代辦事項！');
   }
@@ -52,15 +51,7 @@ function showList(){
     listItem.appendChild(btnRevised);
     listItem.appendChild(btnDelete);
 
-    /***********************************************************************
-    想法順序五
-    1. 只要是物件都可以掛監聽，不是只有已經在 html 上的 DOM 才可掛
-    2. 執行一次 show 方法，只會建立一個 btnDelete 物件跟 listItem 物件，
-       btnDelete 物件掛監聽後，把要刪除的 listItem 物件傳進 deleteItem 方法裡，
-       這裡很清楚是哪個物件(因為不管是 btn 或 li 都只有一個)，不用像之前還要再判斷
-    3. function(e)，console 看 e 是 MouseEvent，
-       這是web api 的 addEventListener 事件對 click 類型做 callback 方法時會傳入的事件
-    ************************************************************************/
+
     btnDelete.addEventListener('click',
     function(e){
       deleteItem(e,listItem);
@@ -91,36 +82,14 @@ function showList(){
     //取得 ul 物件，放入 li 物件(清單項目)
     document.querySelector('.newList').appendChild(listItem);
 
-
-    /***********************************************************************
-    想法順序二
-    1. 最開始用let btnDelete = document.querySelector('.btnDelete');
-       要注意 querySelector 只會抓到第一個刪除紐而已
-    2. 後來用 querySelectorAll 抓到所有刪除鈕，
-       用 forEach 對每個刪除鈕加上監聽事件，再把點擊到的刪除鈕 index 傳到 deleteItem 方法
-    沒考慮到
-    1. 第一次執行 show 方法，index 0 的刪除鈕掛上監聽事件
-    2. 第二次執行 show 方法，index 0 和 index 1 的刪除鈕掛上監聽事件(index 0 掛了兩次)
-    3. 依此類推，許多刪除鈕會重複掛到多次監聽事件
-    ************************************************************************/
-    // let btnDelete = document.querySelectorAll('.btnDelete');
-    // btnDelete.forEach(function(item,index){
-    //   item.addEventListener('click',function(){deleteItem(index)});
-    // });
-
   }//else 結尾
 
   //把代辦事項 input 的文字清空
   document.querySelector('.task').value = "";
 }
 
-/***********************************************************************
-想法順序六
-deleteItem 方法只要把傳進來的 li 物件刪除就好
-************************************************************************/
+
 function deleteItem(e,listItem){
-  // console.log(e);
-  // console.log('listItem',listItem);
   document.querySelector('.newList').removeChild(listItem);
 }
 
@@ -149,41 +118,6 @@ function revisedItem(e,updatelistItem,btnUpdate,btnRevised,listItemText){
   btnRevised.style.display = "none";
 }
 
-/***********************************************************************
-想法順序三
-當刪除鈕按下後會傳入刪除鈕的 index (0,1,2...)，再判斷 li 的 index (0,1,2...)，
-兩者ㄧ樣就刪除
-沒考慮到
-1.當按下 index 0 的刪除鈕，就算刪除 li index 0，
-  但後面 li index 1 會自動往前補變成 li index 0，
-  然後 if 判斷式一直是true，因為liIndex一直都有index 0，直到刪光 index 才會停止
-************************************************************************/
-// function deleteItem(index){
-//   console.log(index);
-//   let listItem = document.querySelectorAll('li');
-//   listItem.forEach(function(item,liIndex){
-//     if(index === liIndex){
-//       document.querySelector('.newList').removeChild(item);
-//     }
-//   });
-// }
-
-/***********************************************************************
-想法順序四
-同上，刪除一個li index，下面的li index 會往前補，if 判斷式一直是true
-************************************************************************/
-// function deleteItem(index){
-//   let listItem = document.querySelectorAll('li');
-//   let found = false;
-//   listItem.forEach(function(item,liIndex){
-//     console.log(item);
-//     if(!found && index === liIndex){
-//       document.querySelector('.newList').removeChild(item);
-//       found = true;
-//     }
-//   });
-// }
-
 
 let btnAdd = document.querySelector('.btnAddNew');
 btnAdd.addEventListener('click',function(e){
@@ -200,16 +134,6 @@ inputNewList.addEventListener('keydown',function(e){
   }
 });
 
-/***********************************************************************
-想法順序ㄧ
-上面的 btnAdd 物件在一開始渲染 html 時就有，所以執行時可以抓到，
-這裡 btnDelete 原本 html 上沒有，所以 console 看 btnDelete 會是 null，
-再執行監聽事件會報錯，所以後來移到 show 方法裡
-************************************************************************/
-// let btnDelete = document.querySelector('.btnDelete');
-// btnDelete.addEventListener('click',deleteItem);
-
-
 function addClassName(checkbox){
   if(checkbox.checked === true){
     checkbox.className = "checkItem checked";
@@ -218,26 +142,26 @@ function addClassName(checkbox){
     checkbox.className = "checkItem unchecked";
   }
 
-  if(status === "All"){
+  if(filterStatus === "All"){
     showAllList();
   }
-  else if(status === "Completed"){
+  else if(filterStatus === "Completed"){
     showCompletedList();
   }
-  else if(status === "InCompleted"){
+  else if(filterStatus === "InCompleted"){
     showInCompletedList();
   }
-  console.log(checkbox.className);
-  console.log(status);
+  // console.log(checkbox.className);
+  // console.log(filterStatus);
 }
 
-var status = "All";
+var filterStatus = "All";
 function showAllList(){
   let allList = document.querySelectorAll('.checkItem');
   allList.forEach(function(item,index){
     item.parentElement.removeAttribute("style");
   });
-  status = "All"
+  filterStatus = "All";
 }
 let btnAll = document.querySelector('.btnAll');
 btnAll.addEventListener('click',function(e){
@@ -258,7 +182,7 @@ function showCompletedList(){
     // console.log(item.parentElement);
     item.parentElement.removeAttribute("style");
   });
-  status = "Completed";
+  filterStatus = "Completed";
 }
 let btnCompleted = document.querySelector('.btnCompleted');
 btnCompleted.addEventListener('click',function(e){
@@ -277,7 +201,7 @@ function showInCompletedList(){
     // console.log(item.parentElement);
     item.parentElement.style.display = "none";
   });
-  status = "InCompleted";
+  filterStatus = "InCompleted";
 }
 let btnInCompleted = document.querySelector('.btnInCompleted');
 btnInCompleted.addEventListener('click',function(e){
