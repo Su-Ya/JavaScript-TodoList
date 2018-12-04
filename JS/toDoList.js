8b826b735038682a4fba09566aa6a09f0397ad62
acid7820@gmail.com <acid7820@gmail.com>
2018-12-04 12:45:38 +0800

    1用preventDefault()移除預設submit/2只出現編輯框裡面有本文/3input框都有enter功能

--- JS/toDoList.js
+++ JS/toDoList.js
@@ -1,39 +1,49 @@
 /*jshint esversion:6*/
 
 function showList(){
+  //建立變數存新增代辦事項 input 的文字
   let addNewListText = document.querySelector('.task').value;
+
+
   if(addNewListText === ""){
     window.alert('请输入代辦事項！');
   }
   else{
+    //建立 checkbox 物件
     let checkbox = document.createElement('input');
     checkbox.type = "checkbox";
     checkbox.className = "checkItem";
     checkbox.name = "check";
 
+    //建立 label 物件用來放代辦事項文字，為搭配 checkbox 出現文字刪除線所以用 label
     let listItemText = document.createElement('label');
     let textNode = document.createTextNode(addNewListText);
     listItemText.htmlFor = "check";
     listItemText.appendChild(textNode);
 
+    //建立 input 物件，用來修改代辦事項文字，一開始先隱藏
     let updatelistItem = document.createElement('input');
     updatelistItem.type = "text";
     updatelistItem.style.visibility = "hidden";
     updatelistItem.className = "inputUpdate";
 
+    //建立 Button 物件，當編輯鈕
     let btnUpdate = document.createElement('button');
     btnUpdate.className = "btnUpdate";
     btnUpdate.textContent = "Edit";
 
+    //建立 Button 物件，當編輯完成鈕，一開始先隱藏
     let btnRevised = document.createElement('button');
     btnRevised.className = "btnRevised";
     btnRevised.style.display = "none";
     btnRevised.textContent = "Revised";
 
+    //建立 Button 物件，當刪除鈕
     let btnDelete = document.createElement('button');
     btnDelete.className = "btnDelete";
     btnDelete.textContent = "Delete";
 
+    //建立 li 物件，當用來清單項目，放入上面建立的物件
     let listItem = document.createElement('li');
     listItem.appendChild(checkbox);
     listItem.appendChild(listItemText);
@@ -58,7 +68,7 @@ function showList(){
 
     btnUpdate.addEventListener('click',
     function(e){
-      updateItem(e,updatelistItem,btnUpdate,btnRevised);
+      updateItem(e,updatelistItem,btnUpdate,btnRevised,listItemText);
     });
 
     btnRevised.addEventListener('click',
@@ -66,7 +76,14 @@ function showList(){
       revisedItem(e,updatelistItem,btnUpdate,btnRevised,listItemText);
     });
 
+    updatelistItem.addEventListener('keydown',function(e){
+      if(e.keyCode === 13){
+        e.preventDefault();
+        revisedItem(e,updatelistItem,btnUpdate,btnRevised,listItemText);
+      }
+    });
 
+    //取得 ul 物件，放入 li 物件(清單項目)
     document.querySelector('.newList').appendChild(listItem);
 
 
@@ -88,6 +105,7 @@ function showList(){
 
   }//else 結尾
 
+  //把代辦事項 input 的文字清空
   document.querySelector('.task').value = "";
 }
 
@@ -101,26 +119,29 @@ function deleteItem(e,listItem){
   document.querySelector('.newList').removeChild(listItem);
 }
 
-function updateItem(e,updatelistItem,btnUpdate,btnRevised){
+//當 btnUpdate clicked 執行 function (編輯狀態)
+function updateItem(e,updatelistItem,btnUpdate,btnRevised,listItemText){
+  updatelistItem.value = listItemText.textContent;
+  // console.log('updateFu',updatelistItem.value );
+  listItemText.style.display = "none";
   updatelistItem.style.visibility = null;
   btnUpdate.style.display = "none";
   btnRevised.style.display = "inline-block";
 }
 
-
+//當 btnRevised clicked 執行 function (編輯完成狀態)
 function revisedItem(e,updatelistItem,btnUpdate,btnRevised,listItemText){
-  let updatelistItemText = updatelistItem.value;
-  if(updatelistItemText === ""){
-    updatelistItem.style.visibility = "hidden";
-    btnUpdate.style.display = "inline-block";
-    btnRevised.style.display = "none";
+  let originalText = listItemText.textContent;
+  if(updatelistItem.value === ""){
+    listItemText.textContent = originalText;
   }
   else{
-    listItemText.textContent = updatelistItemText;
-    updatelistItem.style.visibility = "hidden";
-    btnUpdate.style.display = "inline-block";
-    btnRevised.style.display = "none";
+    listItemText.textContent = updatelistItem.value;
   }
+  listItemText.style.display = "inline";
+  updatelistItem.style.visibility = "hidden";
+  btnUpdate.style.display = "inline-block";
+  btnRevised.style.display = "none";
 }
 
 /***********************************************************************
@@ -159,9 +180,20 @@ function revisedItem(e,updatelistItem,btnUpdate,btnRevised,listItemText){
 // }
 
 
-// let btnAdd = document.querySelector('#addNew');
 let btnAdd = document.querySelector('.btnAddNew');
-btnAdd.addEventListener('click',showList);
+btnAdd.addEventListener('click',function(e){
+  e.preventDefault();
+  showList();
+});
+
+//按下鍵盤 enter 事件
+let inputNewList = document.querySelector('.task');
+inputNewList.addEventListener('keydown',function(e){
+  if( e.keyCode === 13){
+    e.preventDefault();
+    showList();
+  }
+});
 
 /***********************************************************************
 想法順序ㄧ
@@ -171,14 +203,3 @@ btnAdd.addEventListener('click',showList);
 ************************************************************************/
 // let btnDelete = document.querySelector('.btnDelete');
 // btnDelete.addEventListener('click',deleteItem);
-
-
-/***********************************************************************
-按下鍵盤 enter 事件，未完成
-************************************************************************/
-// let addNewListText = document.querySelector('.task');
-// inputaddNewListText.addEventListener('keydown',function(e){
-//   if( e.keyCode === 13){
-
-//   }
-// });
